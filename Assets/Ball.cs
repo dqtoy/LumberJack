6d7cda8e5d088ea629656b9074b178c5fafd352d
Mornslash <sebastian.elf.kulinski@gmail.com>
2018-10-16 13:30:55 +0200

    Launch The Ball

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,18 +8,42 @@ public class Ball : MonoBehaviour
 
     //config parameters
     [SerializeField] Paddle paddle1;
+    [SerializeField] Vector2 launchForce;
+    Rigidbody2D ballRigibody;
 
     // state
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
     // Use this for initialization
     void Start()
     {
+        ballRigibody = GetComponent<Rigidbody2D>();
         paddleToBallVector = transform.position - paddle1.transform.position;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchBall();
+        }
+       
+
+    }
+
+    private void LaunchBall()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ballRigibody.velocity = launchForce;
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
