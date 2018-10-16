c17c5f70895c5c4e7a17991f6ff7f9a00a9e3087
Mornslash <sebastian.elf.kulinski@gmail.com>
2018-10-16 12:30:42 +0200

    Mathf.Clamp for paddle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Paddle : MonoBehaviour {
+public class Paddle : MonoBehaviour
+{
 
-    [SerializeField] float screenWidthInUnits = 16;
+    // configuration parameters
+    [SerializeField] float min = 0.2f;
+    [SerializeField] float max = 6f;
+    [SerializeField] float screenWidthInUnits = 6.25f;
     [SerializeField] Slider moveSlider;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         //float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;        // creating float for actual mouse position
         float sliderInWorldUnits = moveSlider.value * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(sliderInWorldUnits, transform.position.y);
+        paddlePos.x = Mathf.Clamp(sliderInWorldUnits, min, max);
         transform.position = paddlePos;
-	}
+    }
 }
