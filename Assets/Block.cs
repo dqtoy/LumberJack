f8eb1b760699fd134f1df35e7706bad6ce654f00
Mornslash <sebastian.elf.kulinski@gmail.com>
2018-10-23 11:44:43 +0200

    Block Damage Levels

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Block : MonoBehaviour {
-
+public class Block : MonoBehaviour
+{
+    // audio elements
     [SerializeField] AudioClip destoryBlockSound;
     [Range(0, 1)] [SerializeField] float volumeLevel = 1f;
+    // visual efect elements
     [SerializeField] GameObject blockVFX;
     [SerializeField] float vfxLiveTime = 2f;
 
+    // damage processing - block dmg, what sprite etc.
+    [SerializeField] int maxHitsForBlock = 1;
+    [SerializeField] int timesHitAlready = 0; // serialized for debug
+
+
+    // backend elements - scores, counting etc.
     LevelController levelController;
     GameSession gameStatus;
 
@@ -22,15 +30,25 @@ public class Block : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D collision)
     {
         SpawnVFX();
-        DestroyBlock();
-        gameStatus.AddPointsToScore();
+        timesHitAlready++;
+        HandleHit();
+
+    }
+
+    private void HandleHit()
+    {
+        if (timesHitAlready >= maxHitsForBlock)
+        {
+            AudioSource.PlayClipAtPoint(destoryBlockSound, Camera.main.transform.position, volumeLevel);
+            DestroyBlock();
+        }
     }
 
     private void DestroyBlock()
     {
         levelController.destroyedBreakableBlock();
-        AudioSource.PlayClipAtPoint(destoryBlockSound, Camera.main.transform.position, volumeLevel);
         Destroy(gameObject);
+        gameStatus.AddPointsToScore();
     }
 
     private void SpawnVFX()
